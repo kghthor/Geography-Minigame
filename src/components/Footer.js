ea789a9fb6967a88fcd77c1ded71a0dd3ebb5fef
kghthor <harish.1902045@srec.ac.in>
2024-03-23 08:50:20 +0530

    Game Files

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <h1 style={{ marginTop: "20px", marginBottom: "0px" }}>🌐</h1>
+        <h3 style={{ marginTop: "8px" }}>Geography Class Minigame</h3>
+        <h5>Created by Harish K G</h5>
+        <ul>
+          <a title="Github profile" href="https://www.github.com/kghthor">
+            <i className="bi bi-github" />
+          </a>
+          <a title="Send email" href="mailto:playgod378@gmail.com">
+            <i className="bi bi-envelope-fill" />
+          </a>
+        </ul>
+      </footer>
+    </>
+  );
+}
